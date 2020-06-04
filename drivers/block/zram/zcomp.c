817f625dd683ff35823b14b8ff8c2f8ad237ce61
Andy Shevchenko <andriy.shevchenko@linux.intel.com>
2020-06-04 16:49:52 -0700

    BACKPORT: zcomp: Use ARRAY_SIZE() for backends list
    Instead of keeping NULL terminated array switch to use ARRAY_SIZE()
    which helps to further clean up.
    
    Signed-off-by: Andy Shevchenko <andriy.shevchenko@linux.intel.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Reviewed-by: Andrew Morton <akpm@linux-foundation.org>
    Acked-by: Minchan Kim <minchan@kernel.org>
    Cc: Sergey Senozhatsky <sergey.senozhatsky.work@gmail.com>
    Cc: Jens Axboe <axboe@kernel.dk>
    Cc: Andy Shevchenko <andriy.shevchenko@linux.intel.com>
    Link: http://lkml.kernel.org/r/20200508100758.51644-1-andriy.shevchenko@linux.intel.com
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    [ Tashar02: Backport to k4.19 ]
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- drivers/block/zram/zcomp.c
+++ drivers/block/zram/zcomp.c
@@ -19,18 +19,18 @@
 #include "zcomp.h"
 
 static const char * const backends[] = {
-	"lzo",
+	"lzo"
 #if IS_ENABLED(CONFIG_CRYPTO_LZ4)
-	"lz4",
+	"lz4"
 #endif
 #if IS_ENABLED(CONFIG_CRYPTO_LZ4HC)
-	"lz4hc",
+	"lz4hc"
 #endif
 #if IS_ENABLED(CONFIG_CRYPTO_842)
-	"842",
+	"842"
 #endif
 #if IS_ENABLED(CONFIG_CRYPTO_ZSTD)
-	"zstd",
+	"zstd"
 #endif
 };
 
