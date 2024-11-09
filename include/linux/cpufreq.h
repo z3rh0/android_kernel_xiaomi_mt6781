b9c121ef435bb2f94182f6abee9e7835d0ee0fb4
claxten10 <claxten10@gmail.com>
2024-11-09 21:10:02 +0000

    thermal: Import Xiaomi changes
    Signed-off-by: claxten10 <claxten10@gmail.com>

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -428,6 +428,7 @@ static inline void cpufreq_resume(void) {}
 /* Transition notifiers */
 #define CPUFREQ_PRECHANGE		(0)
 #define CPUFREQ_POSTCHANGE		(1)
+#define CPUFREQ_THERMAL                 (2)
 
 /* Policy Notifiers  */
 #define CPUFREQ_ADJUST			(0)
