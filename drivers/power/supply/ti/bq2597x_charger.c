d0f2b07ffe4bcff399d7ad254fac97bea5467f4c
zainarbani <zaintsyariev@gmail.com>
2024-11-20 14:53:39 +0700

    drivers: bq2597x: Fix driver probing issue
    Not sure if interrupt was even used, lets just disabled it for now.
    
    refs:
    https://github.com/MotorolaMobilityLLC/kernel-mtk/blob/MMI-S3RWS32.138-29-5-11/arch/arm64/boot/dts/mediatek/mt6768-corfu-common-overlay.dtsi#L190
    https://github.com/MotorolaMobilityLLC/motorola-kernel-modules/commit/e2c498e0c8ec9914e141b22aa81630c161f68bba
    
    Kernel log:
    7,4797,1638088,-; (0)[1:swapper/0]generic pinconfig core: found bias-pull-up with value 11
    7,4798,1638098,-; (0)[1:swapper/0]generic pinconfig core: found slew-rate with value 0
    7,4799,1638111,-; (0)[1:swapper/0]pinctrl core: add 2 pinctrl maps
    7,4800,1638118,-; (0)[1:swapper/0]generic pinconfig core: found bias-pull-up with value 11
    7,4801,1638126,-; (0)[1:swapper/0]generic pinconfig core: found slew-rate with value 0
    7,4802,1638137,-; (0)[1:swapper/0]pinctrl core: add 2 pinctrl maps
    7,4803,1638160,-; (0)[1:swapper/0]mt6785-pinctrl pinctrl: found group selector 12 for GPIO12
     SUBSYSTEM=platform
     DEVICE=+platform:pinctrl
    7,4804,1638167,-; (0)[1:swapper/0]mt6785-pinctrl pinctrl: found group selector 12 for GPIO12
     SUBSYSTEM=platform
     DEVICE=+platform:pinctrl
    7,4805,1638174,-; (0)[1:swapper/0]mt6785-pinctrl pinctrl: found group selector 12 for GPIO12
     SUBSYSTEM=platform
     DEVICE=+platform:pinctrl
    7,4806,1638180,-; (0)[1:swapper/0]mt6785-pinctrl pinctrl: found group selector 12 for GPIO12
     SUBSYSTEM=platform
     DEVICE=+platform:pinctrl
    7,4807,1638189,-; (0)[1:swapper/0]mt6785-pinctrl pinctrl: request pin 12 (GPIO12) for 7-0065
     SUBSYSTEM=platform
     DEVICE=+platform:pinctrl
    3,4808,1638200,-; (0)[1:swapper/0]mt6785-pinctrl pinctrl: pin_config_group_set op failed for group 12
     SUBSYSTEM=platform
     DEVICE=+platform:pinctrl
    3,4809,1638209,-; (0)[1:swapper/0]bq2597x_charger 7-0065: Error applying setting, reverse things back
     SUBSYSTEM=i2c
     DEVICE=+i2c:7-0065
    4,4810,1638228,-; (0)[1:swapper/0]bq2597x_charger: probe of 7-0065 failed with error -22
    
    Signed-off-by: zainarbani <zaintsyariev@gmail.com>
    Signed-off-by: fjrXTR <fajarslebew31@gmail.com>

--- drivers/power/supply/ti/bq2597x_charger.c
+++ drivers/power/supply/ti/bq2597x_charger.c
@@ -2612,14 +2612,6 @@ static int bq2597x_charger_probe(struct i2c_client *client,
 	struct device_node *node = client->dev.of_node;
 	int ret;
 
-	bq_info("client->irq=%d", client->irq);
-	pr_err("%s: probe start\n", __func__);
-	ret = i2c_smbus_read_byte_data(client, BQ2597X_REG_13);
-	if (ret != BQ25968_DEVICE_ID && ret !=SC8551_DEVICE_ID && ret !=BQ25970_DEVICE_ID && ret != SC8551A_DEVICE_ID) {
-		bq_err("failed to communicate with chip, ret:%d\n", ret);
-		return -ENODEV;
-	}
-	bq_info("bq device id=0x%x\n", ret);
 	bq = devm_kzalloc(&client->dev, sizeof(struct bq2597x), GFP_KERNEL);
 	if (!bq)
 		return -ENOMEM;
